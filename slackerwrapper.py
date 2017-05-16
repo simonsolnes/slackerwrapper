3ab12ea82a5e3da0b69da60bc72427b8fc559bb4
Simon Solnes <simon@solnes.co>
2017-05-16 17:52:07 +0200

    Improvements

--- slackerwrapper.py
+++ slackerwrapper.py
@@ -4,37 +4,27 @@ import threading
 import queue
 import http.client
 
-def internet_on():
-    conn = http.client.HTTPConnection("www.google.com", timeout=5)
-    try:
-        conn.request("HEAD", "/")
-        conn.close()
-        return True
-    except:
-        conn.close()
-        return False
-
-def stop(msg):
-    print(msg)
-    quit()
-
-def db_print(structure):
-    print(json.dumps(structure, indent=4))
 
 class SlackerWrapper():
-    def __init__(self, dbmode = False):
+    def __init__(self):
         try:
-            with open('token', 'r') as f:
-                token = f.readline().strip()
+            with open('token', 'r') as f: token = f.readline().strip()
         except: raise Exception("Document 'token' needs to exist")
 
         self.slacker = Slacker(token)
-        self.fetched = False
-
         self.ch_his = {}
+        self.fetch()
+
+    def has_internet(self):
+        conn = http.client.HTTPConnection("www.google.com", timeout=5)
+        try:
+            conn.request("HEAD", "/")
+            return True
+        except: return False
+        finally: conn.close()
 
     def test_api(self):
-        if not internet_on(): stop("no internet")
+        if not self.has_internet(): raise Exception("no internet")
         data = self.slacker.api.test().body
         if not data or not "ok" in data:
             raise Exception("test did not work")
@@ -42,13 +32,12 @@ class SlackerWrapper():
         else: return True
 
     def fetch(self):
+        if not self.has_internet(): raise Exception("no internet")
         data_queue = queue.Queue()
         threads = []
         threads.append(threading.Thread(target = data_queue.put, args = [self.slacker.users.list().body]))
         threads.append(threading.Thread(target = data_queue.put, args = [self.slacker.channels.list().body]))
         threads.append(threading.Thread(target = data_queue.put, args = [self.slacker.im.list().body]))
-
-        if not internet_on(): stop("no internet")
         for thread in threads: thread.start()
         for thread in threads: thread.join()
         
@@ -68,7 +57,6 @@ class SlackerWrapper():
                     if "name" in channel and "id" in channel:
                         self.channels_id[channel["name"]] = channel["id"]
                         self.channels.append(channel["name"])
-
             if "ims" in data:
                 for im in data["ims"]:
                     if im["user"] in self.users_name:
@@ -80,12 +68,15 @@ class SlackerWrapper():
                     if "name" in user and "id" in user:
                         self.users_name[user["id"]] = user["name"]
                         self.users.append(user["name"])
-        self.fetched = True
 
     def fetch_history(self, channels, count):
+        if not self.has_internet(): raise Exception("no internet")
         data_queue = queue.Queue()
         threads = []
         for channel in channels:
+            if channel not in self.channels:
+                raise Exception("not a channel")
+                
             if channel in self.ims:
                 threads.append(threading.Thread(target = data_queue.put, 
                     args = [{"channel" : channel,
@@ -94,41 +85,32 @@ class SlackerWrapper():
                 threads.append(threading.Thread(target = data_queue.put, 
                     args = [{"channel" : channel,
                     "data" : self.slacker.channels.history(channel = self.channels_id[channel], count = count).body}]))
-            else: raise Exception("not a channel")
-        if not internet_on(): stop("no internet")
         for thread in threads: thread.start()
         for thread in threads: thread.join()
-
         while not data_queue.empty():
             data = data_queue.get()
             channel = data["channel"]
             data = data["data"]
             if not "ok" in data or not data["ok"]: raise Exception("cannot fetch history")
-            hist = []
+            self.ch_his[channel] = []
             if "messages" in data:
                 for message in data["messages"][::-1]:
                     if "user" in message and "text" in message:
-                        hist.append({"name": self.users_name[message["user"]] , "text" : message["text"]})
-            self.ch_his[channel] = hist
+                        self.ch_his[channel].append({"name": self.users_name[message["user"]] , "text" : message["text"]})
 
 
     def send_message(self, channel, text):
-        if channel in self.channels: channel = self.channels_id[channel]
-        else: raise Exception("Trying to send a message to a channelthat does not exist")
+        if not self.has_internet(): raise Exception("no internet")
+        if channel not in self.channels: raise Exception("not a channel")
+        self.channels_id[channel]
         data = self.slacker.chat.post_message( channel = channel, text = text, as_user = True, link_names = True, unfurl_links = False, unfurl_media = False).body
         if not data["ok"]: raise Exception("cannot send message")
 
     def get_users(self):
         return self.users
 
-    def has_user(self, user):
-        return user in self.users
-
     def get_channels(self):
         return self.channels
-
-    def has_channel(self, channel):
-        return channel in self.channels
 
     def get_channel_history(self, channel):
         return self.ch_his[channel]
---
+++ tester.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+import slackerwrapper
+import random
+
+slack = slackerwrapper.SlackerWrapper()
+
+if slack.test_api(): print("api test: OK")
+else: print("api test: NOT OK!!!!")
+
+slack.fetch()
+
+print("users:")
+users = slack.get_users()
+for user in users: print("\t" + user)
+
+print("channels:")
+channels = slack.get_channels()
+for channel in channels: print("\t" + channel)
+
+slack.fetch_history(channels, 5)
+
+for channel in channels:
+    print("channel history for " + channel)
+    for message in slack.get_channel_history(channel):
+        print("\t" + message["name"] + ":\t" + message["text"])
+
+slack.send_message("testing", "test from python: testing unicode: æøååß∂ƒ©˙")
